fb73b00ada5b6b538a6c928b8f090d3b2b22cbfb
YambossMaster <127465994+YambossMaster@users.noreply.github.com>
2023-05-02 12:44:25 +0800

    password.js試驗
    試圖更改button的type變為"submit"

--- public/js/password.js
+++ public/js/password.js
@@ -1,9 +1,15 @@
 var user_account = document.getElementsByClassName("user_account");
 var password = document.getElementsByClassName("password");
 
-
+if(user_account=="a@gmail.com" && password=="123"){
+    const btn = document.querySelector("#btn");
+    btn.setAttribute("type", "submit");
+}
+else{
+    alert("Wrong!!");
+}
 //document.querySelector("#btn").addEventListener("click", function() {window.location.href = "/layout"});
-return false;
+
 /*
 function password_right(user_account,password){
     if(User[user_account]){
