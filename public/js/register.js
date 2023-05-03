d83b4beef0b71939626bf71752590b81aa7e08b3
YambossMaster <127465994+YambossMaster@users.noreply.github.com>
2023-05-04 06:42:18 +0800

    fix register page and fix verification code
    重製register page排版，驗證碼生成與更新

--- public/js/register.js
+++ public/js/register.js
@@ -1,10 +1,15 @@
 const form = document.querySelector('#register-form');
-const usernameInput = document.querySelector('#username');
+const usernameInput = document.querySelector('#user_name');
+const emailInput = document.querySelector('#email');
 const passwordInput = document.querySelector('#password');
+const repasswordInput = document.querySelector('#repassword');
+const birth_dInput = document.querySelector('#birth_d');
+const birth_mInput = document.querySelector('#birth_m');
+const birth_yInput = document.querySelector('#birth_y');
 const registerBtn = document.querySelector('#register-btn');
 
 const codeInput = document.querySelector('.code');
-const codeButton = document.querySelector('#code');
+const codeDisplay = document.querySelector('#code');
 
 let code = '';
 
@@ -20,24 +25,38 @@ function generateCode() {
 // 更換驗證碼
 function change() {
     code = generateCode();
-    codeInput.value = code;
+    codeDisplay.value = code;
 }
 
 //創建第一組驗證碼
 change();
-
+codeDisplay.addEventListener('click',function(){
+  change();
+})
 // 提交表單時進行驗證
 form.addEventListener('submit', function(event) {
   event.preventDefault();
   const username = usernameInput.value;
   const password = passwordInput.value;
+  const repassword = repasswordInput.value;
+  const email = emailInput.value;
+  const birth_d = birth_dInput.value;
+  const birth_m = birth_mInput.value;
+  const birth_y = birth_yInput.value;
+
   const inputCode = codeInput.value;
-  if (code && inputCode.toLowerCase() === code.toLowerCase()) {
+
+  /*password and repassword*/
+  if(password != repassword){
+    alert('密碼前後不一致，請確認後再次輸入。');
+    repasswordInput.value = '';
+  }
+  /*verification code*/
+  if (code && inputCode.toLowerCase() == code.toLowerCase()) {
     document.getElementById('btn').type = 'submit';
     form.submit();
   } else {
     alert('驗證碼不正確!!請在試一次。');
-    passwordInput.value = '';
     codeInput.value = '';
     change();
   }
