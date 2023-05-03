c1f4db3cacb99c8379cc82cc47d87b2b22938d7c
XChiChiX <bob1993.chg@gmail.com>
2023-05-03 22:29:01 +0800

    fix login.ejs, password.js
    可判斷輸入帳密是否存在於User table 中
    目前table只有一筆資料 {email : "homo@aaaaa", password : "114514"...}

--- public/js/password.js
+++ public/js/password.js
@@ -17,22 +17,38 @@ form.addEventListener('submit', function(event) {
   }
 });
 */
-const form = document.querySelector('form');
-const usernameInput = document.querySelector('input[name="login[username]"]');
-const passwordInput = document.querySelector('input[name="login[password]"]');
+// const users = <%- JSON.stringify(user) %>;
+const loginForm = document.getElementById("login-form");
+// const usernameInput = document.querySelector('input[name="login[username]"]');
+// const passwordInput = document.querySelector('input[name="login[password]"]');
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  const username = usernameInput.value;
-  const password = passwordInput.value;
-  const user = users.find(u => u.email === username);
-  if (user && user.password === password) {
-    document.getElementById('btn').type = 'submit';
-    form.submit();
-  } 
-  else {
-    alert('帳號或密碼不正確!!請在試一次。');
-    passwordInput.value = '';
+loginForm.addEventListener('submit', function(event) {
+  // event.preventDefault();
+  let loginSuccess = false;
+  const username = document.getElementById("username");
+  const password = document.getElementById("password");
+  // const user = users.find(u => u.email === username);
+  // if (user && user.password === password) {
+  //   document.getElementById('btn').type = 'submit';
+  //   form.submit();
+  // } 
+  for(let i = 0; i < users.length; ++i){
+    if(username.value === users[i].email && password.value === users[i].password){
+      loginSuccess = true;
+      break;
+    }
   }
+  if(!loginSuccess){
+    alert('帳號或密碼不正確!!請在試一次。');
+    password.value = "";
+    event.preventDefault();
+  }
+  // if(username.value != users[0].email || password.value != users[0].password){
+  //   alert('帳號或密碼不正確!!請在試一次。');
+  //   // passwordInput.value = '';
+  //   // loginForm.reset();
+  //   password.value = "";
+  //   event.preventDefault();
+  // }
 });
 
